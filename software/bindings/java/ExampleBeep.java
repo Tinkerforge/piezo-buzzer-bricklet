c92695bb3a999217ff22515863594d94d86c2e88
Olaf Lüke <olaf@tinkerforge.com>
2011-09-13 19:04:28 +0200

    add java bindings and examples

---
+++ software/bindings/java/ExampleBeep.java
@@ -0,0 +1,25 @@
+import com.tinkerforge.BrickletPiezoBuzzer;
+import com.tinkerforge.IPConnection;
+
+public class ExampleBeep {
+	private static final String host = new String("localhost");
+	private static final int port = 4223;
+	private static final String UID = new String("ABC"); // Change to your UID
+	
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the commnents below
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(host, port); // Create connection to brickd (Can throw IOException)
+
+		BrickletPiezoBuzzer pb = new BrickletPiezoBuzzer(UID); // Create device object
+		ipcon.addDevice(pb); // Add device to ip connection (Can throw IPConnection.TimeoutException)
+		// Don't use device before it is added to a connection
+		
+
+		// Make 2 second beep
+		pb.beep(2000);
+		
+		System.out.println("Press ctrl+c to exit");
+		ipcon.joinThread();
+	}
+}
