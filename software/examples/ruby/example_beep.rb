48e965a73873ba441ed1962363ab87a13aa3ae5a
Matthias Bolte <matthias@tinkerforge.com>
2012-05-25 12:40:03 +0200

    Add Ruby examples

---
+++ software/examples/ruby/example_beep.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# -*- ruby encoding: utf-8 -*-
+
+require 'tinkerforge/ip_connection'
+require 'tinkerforge/bricklet_piezo_buzzer'
+
+include Tinkerforge
+
+HOST = 'localhost'
+PORT = 4223
+UID = '6tU' # Change to your UID
+
+ipcon = IPConnection.new HOST, PORT # Create IP connection to brickd
+pb = BrickletPiezoBuzzer.new UID # Create device object
+ipcon.add_device pb # Add device to IP connection
+# Don't use device before it is added to a connection
+
+# Make 2 second beep
+pb.beep 2000
+
+puts 'Press key to exit'
+$stdin.gets
+ipcon.destroy
