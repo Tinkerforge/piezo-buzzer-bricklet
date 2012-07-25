381ad787bdfa6acc3f842f6ab127b7b8817e40cf
Matthias Bolte <matthias@tinkerforge.com>
2012-07-25 13:34:11 +0200

    Add Delphi examples

---
+++ software/examples/delphi/ExampleBeep.pas
@@ -0,0 +1,50 @@
+program ExampleBeep;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletPiezoBuzzer;
+
+type
+  TExample = class
+  private
+    ipcon: TIPConnection;
+    pb: TBrickletPiezoBuzzer;
+  public
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = '6tU';
+
+var
+  e: TExample;
+
+procedure TExample.Execute;
+begin
+  { Create IP connection to brickd }
+  ipcon := TIPConnection.Create(HOST, PORT);
+
+  { Create device object }
+  pb := TBrickletPiezoBuzzer.Create(UID);
+
+  { Add device to IP connection }
+  ipcon.AddDevice(pb);
+  { Don't use device before it is added to a connection }
+
+  { Make 2 second beep }
+  pb.Beep(2000);
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy;
+end;
+
+begin
+  e := TExample.Create;
+  e.Execute;
+  e.Destroy;
+end.
