17b183a8a8c5907539f3a3112ca4ed33ecb0d9f2
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 13:22:23 +0100

    Make C examples Protocol 2.0 compatible

--- software/examples/c/example_beep.c
+++ software/examples/c/example_beep.c
@@ -9,23 +9,20 @@
 #define UID "XYZ" // Change to your UID
 
 int main() {
-	// Create IP connection to brickd
+	// Create IP connection
 	IPConnection ipcon;
-	if(ipcon_create(&ipcon, HOST, PORT) < 0) {
-		fprintf(stderr, "Could not create connection\n");
-		exit(1);
-	}
+	ipcon_create(&ipcon);
 
 	// Create device object
 	PiezoBuzzer pb;
-	piezo_buzzer_create(&pb, UID); 
+	piezo_buzzer_create(&pb, UID, &ipcon); 
 
-	// Add device to IP connection
-	if(ipcon_add_device(&ipcon, &pb) < 0) {
-		fprintf(stderr, "Could not connect to Bricklet\n");
+	// Connect to brickd
+	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
+		fprintf(stderr, "Could not connect\n");
 		exit(1);
 	}
-	// Don't use device before it is added to a connection
+	// Don't use device before ipcon is connected
 
 	// Make 2 second beep
 	piezo_buzzer_beep(&pb, 2000);
