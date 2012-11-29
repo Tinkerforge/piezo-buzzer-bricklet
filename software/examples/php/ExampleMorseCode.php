2da91d986a1545f582aed9baacfbcd8668309a81
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 17:44:14 +0100

    Make PHP examples Protocol 2.0 compatible

--- software/examples/php/ExampleMorseCode.php
+++ software/examples/php/ExampleMorseCode.php
@@ -10,17 +10,16 @@ $host = 'localhost';
 $port = 4223;
 $uid = '6tU'; // Change to your UID
 
-$ipcon = new IPConnection($host, $port); // Create IP connection to brickd
-$pb = new BrickletPiezoBuzzer($uid); // Create device object
+$ipcon = new IPConnection(); // Create IP connection
+$pb = new BrickletPiezoBuzzer($uid, $ipcon); // Create device object
 
-$ipcon->addDevice($pb); // Add device to IP connection
-// Don't use device before it is added to a connection
+$ipcon->connect($host, $port); // Connect to brickd
+// Don't use device before ipcon is connected
 
 // Morse SOS
 $pb->morseCode('... --- ...');
 
 echo "Press key to exit\n";
 fgetc(fopen('php://stdin', 'r'));
-$ipcon->destroy();
 
 ?>
