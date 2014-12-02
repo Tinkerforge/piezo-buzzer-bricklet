a226a4a6d0cb52b31f723a2aaf01f39bbc9f358e
Olaf Lüke <olaf@tinkerforge.com>
2014-12-02 17:09:14 +0100

    Use ReadLine instead of ReadKey for C# example

--- software/examples/csharp/ExampleBeep.cs
+++ software/examples/csharp/ExampleBeep.cs
@@ -17,8 +17,8 @@ class Example
 		// Make 2 second beep
 		pb.Beep(2000);
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
--- software/examples/csharp/ExampleMorseCode.cs
+++ software/examples/csharp/ExampleMorseCode.cs
@@ -17,8 +17,8 @@ class Example
 		// Morse SOS
 		pb.MorseCode("... --- ...");
 
-		System.Console.WriteLine("Press key to exit");
-		System.Console.ReadKey();
+		System.Console.WriteLine("Press enter to exit");
+		System.Console.ReadLine();
 		ipcon.Disconnect();
 	}
 }
