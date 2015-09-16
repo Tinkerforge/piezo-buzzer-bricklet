1518b715edcd14523985dc1fadd5e64f81057fd7
Matthias Bolte <matthias@tinkerforge.com>
2015-09-16 18:33:33 +0200

    Add JavaScript examples

---
+++ software/examples/javascript/ExampleMorseCode.js
@@ -0,0 +1,30 @@
+var Tinkerforge = require('tinkerforge');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'XYZ'; // Change to your UID
+
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var pb = new Tinkerforge.BrickletPiezoBuzzer(UID, ipcon); // Create device object
+
+ipcon.connect(HOST, PORT,
+    function (error) {
+        console.log('Error: ' + error);
+    }
+); // Connect to brickd
+// Don't use device before ipcon is connected
+
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
+    function (connectReason) {
+        // Morse SOS
+        pb.morseCode('... --- ...');
+    }
+);
+
+console.log('Press key to exit');
+process.stdin.on('data',
+    function (data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
