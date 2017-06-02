a788135ae3b81c5e58130b5acab2267f2b7a84c8
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:22:05 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_beep.m
+++ software/examples/matlab/octave_example_beep.m
@@ -5,8 +5,8 @@ function octave_example_beep()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your Piezo Buzzer Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    pb = java_new("com.tinkerforge.BrickletPiezoBuzzer", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    pb = javaObject("com.tinkerforge.BrickletPiezoBuzzer", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
