07524fc982d1a517f050826b10d83e2f34159d9c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-06 15:57:04 +0100

    Add rust examples

---
+++ software/examples/rust/example_beep.rs
@@ -0,0 +1,24 @@
+use std::{error::Error, io};
+
+use tinkerforge::{ipconnection::IpConnection, piezo_buzzer_bricklet::*};
+
+const HOST: &str = "127.0.0.1";
+const PORT: u16 = 4223;
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Piezo Buzzer Bricklet
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ipcon = IpConnection::new(); // Create IP connection
+    let piezo_buzzer_bricklet = PiezoBuzzerBricklet::new(UID, &ipcon); // Create device object
+
+    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
+                                        // Don't use device before ipcon is connected
+
+    // Make 2 second beep
+    piezo_buzzer_bricklet.beep(2000);
+
+    println!("Press enter to exit.");
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input)?;
+    ipcon.disconnect();
+    Ok(())
+}
