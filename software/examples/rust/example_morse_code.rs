584429442efa097b5d56489f9a150aa1b6a272a2
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:25:48 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_morse_code.rs
+++ software/examples/rust/example_morse_code.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, piezo_buzzer_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, piezo_buzzer_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Piezo Buzzer Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Piezo Buzzer Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let piezo_buzzer_bricklet = PiezoBuzzerBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let pb = PiezoBuzzerBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Morse SOS
-    piezo_buzzer_bricklet.morse_code("... --- ...".to_string());
+    pb.morse_code("... --- ...".to_string());
 
     println!("Press enter to exit.");
     let mut _input = String::new();
