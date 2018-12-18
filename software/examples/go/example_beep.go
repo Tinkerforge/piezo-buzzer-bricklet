73912e14f0a8b85964d44d32ac092bf743131a59
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:59:05 +0100

    go: Change import path.

--- software/examples/go/example_beep.go
+++ software/examples/go/example_beep.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/piezo_buzzer_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/piezo_buzzer_bricklet"
 )
 
 const ADDR string = "localhost:4223"
