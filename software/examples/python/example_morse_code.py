a3e3eb3215ad8f29da8e3924a08f89d594d9c7ac
Erik Fleckstein <erik@tinkerforge.com>
2020-01-13 17:21:45 +0100

    examples: Use python3 input instead of python2 raw_input.

--- software/examples/python/example_morse_code.py
+++ software/examples/python/example_morse_code.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     # Morse SOS
     pb.morse_code("... --- ...")
 
-    raw_input("Press key to exit\n") # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
